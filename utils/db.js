86c378bb510a47d2bcb8b538dd3ed08dcd06d5a9
LuisAcevedo777 <leavleav01@gmail.com>
2024-09-07 22:39:25 -0500

    server uptaded1

--- utils/db.js
+++ utils/db.js
@@ -1,14 +1,17 @@
 const {Sequelize} = require('sequelize')
-
-const db = new Sequelize({
-
+require('dotenv').config()
+const db = new Sequelize(process.env.DATABASE_URL,{
+    
     port: 5432,
     database: 'klubserver',
-    username: 'postgres',
-    password: 'root',
+    username: 'klubserver_user',
+    password: 'gKDEGOzY4LQclYPhqs0b6w2sccUSsOaQ',
     dialect: 'postgres',
-    host: 'localhost'
+    host: 'dpg-cregosrgbbvc73bqqdn0-a.oregon-postgres.render.com',
+    logging: false
 
 })
 
 module.exports = db
+
+/*postgresql://klubserver_user:gKDEGOzY4LQclYPhqs0b6w2sccUSsOaQ@dpg-cregosrgbbvc73bqqdn0-a.oregon-postgres.render.com/klubserver*/
