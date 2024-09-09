3e4476dbede6d556a63cfb998f0f5987b827fd41
LuisAcevedo777 <leavleav01@gmail.com>
2024-09-08 20:09:42 -0500

    server updated11

--- utils/db.js
+++ utils/db.js
@@ -5,7 +5,7 @@ const db = new Sequelize(process.env.DATABASE_URL,{
     port: 5432,
     database: 'railway', //'klubserver',
     username: 'postgres',
-    password: 'FNQscGuLKlFDKyZgqoWAMgACipZupMkL',//'root', 
+    password: 'YzzvfsBqepLlZKVgIuEXKUFiwEJyZOpZ',//'root', 
     dialect: 'postgres',
     host: 'meticulous-empathy.railway.internal', // 'localhost',
     logging: false
